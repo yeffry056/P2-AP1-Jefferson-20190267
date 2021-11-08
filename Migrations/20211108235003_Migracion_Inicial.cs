b62e73e6ecf9a38400b6fb67bd766083f42e10f4
Jefferson Javier Vasquez <jefferson_javier@ucne.edu.do>
2021-11-08 19:50:48 -0400

    Creacion de Botones

---
+++ Migrations/20211108235003_Migracion_Inicial.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P2_AP1_Jefferson_20190267.Migrations
+{
+    public partial class Migracion_Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Proyecto",
+                columns: table => new
+                {
+                    ProyectoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Proyecto", x => x.ProyectoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TipoTarea",
+                columns: table => new
+                {
+                    TipoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TipoTareaa = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoTarea", x => x.TipoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DetalleTarea",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Requerimiento = table.Column<string>(type: "TEXT", nullable: true),
+                    Tiempo = table.Column<int>(type: "INTEGER", nullable: false),
+                    TiempoTotal = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProyectoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    TipoId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DetalleTarea", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DetalleTarea_Proyecto_ProyectoId",
+                        column: x => x.ProyectoId,
+                        principalTable: "Proyecto",
+                        principalColumn: "ProyectoId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DetalleTarea_TipoTarea_TipoId",
+                        column: x => x.TipoId,
+                        principalTable: "TipoTarea",
+                        principalColumn: "TipoId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TipoTarea",
+                columns: new[] { "TipoId", "TipoTareaa" },
+                values: new object[] { 1, "Analisis" });
+
+            migrationBuilder.InsertData(
+                table: "TipoTarea",
+                columns: new[] { "TipoId", "TipoTareaa" },
+                values: new object[] { 2, "Diseño" });
+
+            migrationBuilder.InsertData(
+                table: "TipoTarea",
+                columns: new[] { "TipoId", "TipoTareaa" },
+                values: new object[] { 3, "Programacion" });
+
+            migrationBuilder.InsertData(
+                table: "TipoTarea",
+                columns: new[] { "TipoId", "TipoTareaa" },
+                values: new object[] { 4, "Prueba" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetalleTarea_ProyectoId",
+                table: "DetalleTarea",
+                column: "ProyectoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetalleTarea_TipoId",
+                table: "DetalleTarea",
+                column: "TipoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DetalleTarea");
+
+            migrationBuilder.DropTable(
+                name: "Proyecto");
+
+            migrationBuilder.DropTable(
+                name: "TipoTarea");
+        }
+    }
+}
--- UI/Registros/rTarea.xaml.cs
+++ UI/Registros/rTarea.xaml.cs
@@ -21,20 +21,105 @@ namespace P2_AP1_Jefferson_20190267.UI.Registros
     /// </summary>
     public partial class rTarea : Window
     {
-        
-      
+
+        private Proyecto proyecto = new Proyecto();
         public rTarea()
         {
             InitializeComponent();
+            this.DataContext = proyecto;
             
-
-
             TareaComboBox.ItemsSource = TipoTareaBLL.GetTipoTarea();
             TareaComboBox.SelectedValuePath = "TipoId";
             TareaComboBox.DisplayMemberPath = "TipoTareaa";
         }
-        
+        private void Cargar()
+        {
+            this.DataContext = null;
+            this.DataContext = proyecto;
 
+        }
+        private void Limpiar()
+        {
+            this.proyecto = new Proyecto();
+            this.DataContext = proyecto;
+        }
+        private void BtnBuscar(object sender, RoutedEventArgs e)
+        {
+            var encontrado = ProyectoBLL.Buscar(proyecto.ProyectoId);
 
+            if(encontrado != null)
+            {
+                proyecto = encontrado;
+                Cargar();
+            }
+            else
+            {
+                Limpiar();
+                MessageBox.Show("Proyecto no existe", "Fallo", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void BtnAgregarFila(object sender, RoutedEventArgs e)
+        {
+            proyecto.Detalle.Add(new DetalleTarea
+            {
+                ProyectoId = proyecto.ProyectoId,
+                tipoTarea = (TipoTarea)TareaComboBox.SelectedItem,
+                Tiempo = Utilidades.ToInt(TextMinutos.Text),
+                TiempoTotal = proyecto.Detalle.Sum(e => e.Tiempo),
+                Requerimiento = TextRequerimiento.Text
+            }) ;
+
+            Cargar();
+
+            TextRequerimiento.Focus();
+            TextRequerimiento.Clear();
+            TextMinutos.Clear();
+           
+        }
+
+        private void BtnRemoverFila(object sender, RoutedEventArgs e)
+        {
+            if(DetalleDataGrid.Items.Count >= 1 && DetalleDataGrid.SelectedIndex <= DetalleDataGrid.Items.Count - 1)
+            {
+                proyecto.Detalle.RemoveAt(DetalleDataGrid.SelectedIndex);
+                Cargar();
+            }
+        }
+
+        private void BtnNuevo(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void BtnGuardar(object sender, RoutedEventArgs e)
+        {
+            var paso = ProyectoBLL.Guardar(proyecto);
+
+            if (paso)
+            {
+                Limpiar();
+                MessageBox.Show("Guardado!", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("No se pudo guardar", "Fallo", MessageBoxButton.OK, MessageBoxImage.Error);
+
+        }
+
+        private void BtnEliminar(object sender, RoutedEventArgs e)
+        {
+            var existe = ProyectoBLL.Buscar(proyecto.ProyectoId);
+
+            if(existe == null)
+            {
+                MessageBox.Show("No existe el proyecto", "Fallo", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                ProyectoBLL.Eliminar(proyecto.ProyectoId);
+                MessageBox.Show("Eliminado", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            }
+        }
     }
 }
