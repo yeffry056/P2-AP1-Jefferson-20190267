d382b743bdc98f0314e7562229729a67311011a5
Jefferson Javier Vasquez <jefferson_javier@ucne.edu.do>
2021-11-08 18:26:13 -0400

    Creacion de Entidades

---
+++ Entidades/DetalleTarea.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_AP1_Jefferson_20190267.Entidades
+{
+    public class DetalleTarea
+    {
+        [Key]
+        public int Id { get; set; }
+        public String Requerimiento { get; set; }
+        public int Tiempo { get; set; }
+        public int TiempoTotal { get; set; }
+        public int TareaId { get; set; }
+
+        [ForeignKey("TipoId")]
+        public TipoTarea tipoTarea { get; set; }
+    }
+}
---
+++ Entidades/Tarea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_AP1_Jefferson_20190267.Entidades
+{
+    public class Tarea
+    {
+        [Key]
+        public int TareaId { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public String Descripcion { get; set; }
+
+        [ForeignKey("TareaId")]
+       // public List<> MyProperty { get; set; }
+    }
+}
